140fe1992a05e307a11acf209ace394d80927eeb
Eason Huang <easonh887@gmail.com>
2025-03-22 21:03:30 -0400

    bruhski

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}', // Ensure this matches the structure of your project
